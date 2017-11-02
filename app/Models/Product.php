d5b4e6f38ff413644ad0760c364235334ab8c11a
Andrey K <andrey.kurmel@faceit-team.com>
2017-11-02 13:51:48 +0200

    Work with Accessors & Mutators for change column 'description' to new column 'description2'.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,17 @@ class Product extends Model
 {
     protected $fillable = ['name', 'code', 'description', 'inStock', 'price', 'userId'];
 
+    public function getDescriptionAttribute() {
+        return $this->description2;
+    }
+
+    public function setDescriptionAttribute($value) {
+        $this->description2 = $value;
+        $this->attributes['description'] = "";
+    }
+
+    protected $visible = ['name', 'description', 'price'];
+
     public function user() {
         return $this->belongsTo(User::class, 'userId', 'id');
     }
